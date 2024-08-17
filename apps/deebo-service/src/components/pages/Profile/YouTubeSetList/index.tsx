823da50a5de8b85e27225eaff9915f89956343d1
stardust6653 <stardust6653@gmail.com>
2024-08-18 02:00:41 +0900

    feat : 유튜브 리스트 height 절대값으로 변경

--- apps/deebo-service/src/components/pages/Profile/YouTubeSetList/index.tsx
+++ apps/deebo-service/src/components/pages/Profile/YouTubeSetList/index.tsx
@@ -14,14 +14,38 @@ interface Props {
 
 const YouTubePlayList = ({ playList, setMainVideoId, height }: Props) => {
   return (
-    <div className={`w-full ml-2 h-[${height}px] `}>
+    // <div className={`w-full ml-2 h-[${height}px] `}>
+    //   <p className="font-bold mb-2 pl-2">재생목록</p>
+    //   <div className={`overflow-scroll scrollbar-hide h-[calc(100%-32px)]`}>
+    //     <ul className="border rounded-lg overflow-y-auto flex flex-col">
+    //       {playList.map((video, index) => (
+    //         <li
+    //           key={index}
+    //           className="border p-2 px-4 flex justify-between items-center cursor-pointer hover:bg-gray-100"
+    //           onClick={() => setMainVideoId(video.videoId)}
+    //         >
+    //           <div>
+    //             <h3 className="truncate font-bold">{video.title}</h3>
+    //             <div className="">{video.artist}</div>
+    //           </div>
+    //           <div className="relative">
+    //             <div className="inset-0 w-[100px] h-full pointer-events-none">
+    //               <YouTubePreview videoId={video.videoId} type="playlist" />
+    //             </div>
+    //           </div>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </div>
+    <div className={`w-[50%] ml-2 h-[348}px] `}>
       <p className="font-bold mb-2 pl-2">재생목록</p>
-      <div className={`overflow-scroll scrollbar-hide h-[calc(100%-32px)]`}>
-        <ul className="border rounded-lg overflow-y-auto flex flex-col">
+      <div className={`overflow-scroll scrollbar-hide h-[316px]`}>
+        <ul className="border rounded-lg overflow-y-auto flex flex-col h-full">
           {playList.map((video, index) => (
             <li
               key={index}
-              className="border p-2 px-4 flex justify-between items-center cursor-pointer hover:bg-gray-100"
+              className="h-[79px] border-b p-2 px-4 flex justify-between items-center cursor-pointer hover:bg-gray-100"
               onClick={() => setMainVideoId(video.videoId)}
             >
               <div>
